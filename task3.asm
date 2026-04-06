1d6cc2dec126578085a5f345e5750a1c9ae96476
zakaria <himranezakaria2@gmail.com>
2026-04-06 16:47:35 +0100

    fix: task3 issue

--- task3.asm
+++ task3.asm
@@ -5,14 +5,14 @@ tache3 PROC
 
 parcours:
     MOV AL, matrix[SI]  ; on charge l element courant dans AL
-    CMP AL, 5           ; on compare avec 5
-    JL trouver          ; si AL < 5 on saute vers trouver
+    CMP AL, '5'         ; on compare avec '5'
+    JL trouver          ; si AL < '5' on saute vers trouver
 
-    MOV matrix[SI], 1   ; on ecrit 1 a la place
+    MOV matrix[SI], '1' ; on ecrit '1' a la place
     JMP etiq
 
 trouver:
-    MOV matrix[SI], 0   ; on ecrit 0 a la place
+    MOV matrix[SI], '0' ; on ecrit '0' a la place
 
 etiq:
     INC SI              ; on passe a l element suivant
